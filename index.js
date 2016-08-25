cf9840715742e5c6af796fa4daf679f70e74e9e7
doowb <brian.woodward@gmail.com>
2016-08-25 00:22:02 -0400

    refactor to be an actual gulp plugin and require a file to be piped to it get a list of webtask modules and exclude them all from browserify

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 'use strict';
 
 var path = require('path');
-var find = require('find-pkg');
-var through = require('through2');
-var uglify = require('gulp-uglify');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var extend = require('extend-shallow');
-var source = require('vinyl-source-stream');
+var utils = require('./lib/utils');
 
 /**
  * Use [browserify][], [gulp-uglify][], and defaults for the browserify commandline `node` flag to bundle source code for a [webtask][]
@@ -27,15 +21,16 @@ var source = require('vinyl-source-stream');
  * @api public
  */
 
-module.exports = function bundle(filepath, filename, options) {
+module.exports = function bundle(filename, options) {
   if (typeof filename === 'object') {
     options = filename;
-    filename = 'bundle.js';
+    filename = null;
   }
 
+  filename = filename || 'bundle.js';
   options = options || {};
 
-  var pkg = require(find.sync(options.cwd || process.cwd()));
+  var pkg = require(utils.find.sync(options.cwd || process.cwd()));
   var defaults = {
     standalone: pkg.name,
     browserField: false,
@@ -49,17 +44,45 @@ module.exports = function bundle(filepath, filename, options) {
     }
   };
 
-  var opts = extend({}, defaults, options);
-  var cwd = opts.cwd || process.cwd();
+  var opts = utils.extend({}, defaults, options);
 
-  return browserify(path.resolve(cwd, filepath), opts)
-    .exclude('request') // expand this
-    .bundle()
-    .pipe(source(filename, cwd))
-    .pipe(buffer())
-    .pipe(uglify())
-    .pipe(through.obj(function(file, enc, next) {
-      file.contents = new Buffer('exports = module.exports;\n' + file.contents.toString());
-      next(null, file);
-    }));
+  var filepath, base, excluded;
+  return utils.through.obj(function(file, enc, cb) {
+    filepath = file.path;
+    base = file.base;
+    utils.getModules(function(err, modules) {
+      if (err) return cb(err);
+      excluded = modules.filter(function(mod) {
+        return mod.version !== 'native';
+      }).map(function(mod) {
+        return mod.name;
+      });
+      cb();
+    });
+  }, function(cb) {
+    var res = null;
+
+    // create browserify instance
+    var b = utils.browserify(filepath, opts);
+
+    // exclude modules included with webtask
+    b = excluded.reduce(function(acc, mod) {
+      acc.exclude(mod);
+      return acc;
+    }, b);
+
+    // bundle
+    b.bundle()
+      .pipe(utils.source(filename, base))
+      .pipe(utils.buffer())
+      .pipe(utils.uglify())
+      .pipe(utils.through.obj(function(file, enc, next) {
+        file.contents = new Buffer('exports = module.exports;\n' + file.contents.toString());
+        res = file;
+        next();
+      }, function(next) {
+        cb(null, res);
+        next();
+      }));
+  });
 };
